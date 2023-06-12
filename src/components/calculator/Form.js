9e46a00a642873bd7e162779e0cb441aaee12fe8
soype <phenestroza@gmail.com>
2023-06-12 01:28:30 -0300

    Add fixed rate logic

--- src/components/calculator/Form.js
+++ src/components/calculator/Form.js
@@ -4,7 +4,7 @@ import styles from "./Form.module.css";
 import Card from "../UI/Card";
 import Result from "./Result";
 
-const Form = () => {
+const Form = (props) => {
   const [tasa, setTasa] = useState();
   const [tasaInt, setTasaInt] = useState(0);
   const [resultado, setResultado] = useState("");
@@ -13,28 +13,7 @@ const Form = () => {
   const [cantCuotas, setCantCuotas] = useState(0);
   const [valorFinal, setValorFinal] = useState(0);
 
-  const apiToken = process.env.REACT_APP_API_TOKEN;
-
-  const apiUrl = "tasa_depositos_30_dias";
-  const proxyUrl = "https://bcra-proxy-cors.vercel.app";
-
-  useEffect(() => {
-    fetch(`${proxyUrl}/${apiUrl}`, {headers:{
-      Authorization: `${apiToken}`,
-    }})
-    .then((res) => res.json())
-    .then(data => {
-      const lastItem = data[data.length - 1]; // Access the last item in the data array
-      setTasa(lastItem.v / 100); // Process the last item as needed
-    })
-    .catch(error => console.log(error))
-  }, [resultado])
-
-
-  // Fallback por si falla la API de BCRA.
-  useEffect(() => {
-    setTasa(0.9299);
-  })
+  
 
   useEffect(() => {
     setValorFinal(valorCuota * cantCuotas);
@@ -77,10 +56,6 @@ const Form = () => {
     setCantCuotas(e.currentTarget.value);
   };
 
-  const valorFinalHandler = (e) => {
-    setValorFinal(e.currentTarget.value);
-  };
-
   return (
     <Card className={styles.form}>
       <form onSubmit={(event) => event.preventDefault()} className={styles.calculator}>
@@ -95,19 +70,14 @@ const Form = () => {
           onChange={valorInicialHandler}
         />
         <p>
-          Ingresá el valor total con cuotas o bien la cuota que pagarías para
-          calcular el CFT resultante y ver si te conviene la compra.
+          Ingresá la cuota que pagarías para calcular si te conviene hacer la compra en un pago o en cuotas.
         </p>
         <label htmlFor="">Valor de cada cuota</label>
         <input type="number" placeholder={valorCuota} onChange={valorCuotaHandler} />
         <label htmlFor="">Cantidad de cuotas</label>
         <input type="number" placeholder={cantCuotas} onChange={cantCuotasHandler} />
-        <label htmlFor="">Valor total con cuotas</label>
-        <input type="number" value={valorFinal} onChange={valorFinalHandler} />
-        <p>Tasa de interés: {tasaInt * 100}%</p>
-        <p>Tasa plazo fijo: {tasa * 100}%</p>
       </form>
-      <Result resultado={resultado}></Result>
+      <Result resultado={resultado} valorFinal={valorFinal} interes={tasaInt * 100} plazoFijo={tasa * 100} tasaPlazoHandler={setTasa}></Result>
     </Card>
   );
 };
--- src/components/calculator/Result.js
+++ src/components/calculator/Result.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, {useEffect} from "react";
 
-import styles from './Result.module.css';
+import styles from "./Result.module.css";
 
 const Result = (props) => {
-  return (
-    <div className={styles.resultado}>{props.resultado}</div>
-  )
-}
 
-export default Result
+  const apiToken = process.env.REACT_APP_API_TOKEN;
+
+  const apiUrl = "tasa_depositos_30_dias";
+  const proxyUrl = "https://bcra-proxy-cors.vercel.app";
+
+  useEffect(() => {
+    fetch(`${proxyUrl}/${apiUrl}`, {headers:{
+      Authorization: `${apiToken}`,
+    }})
+    .then((res) => res.json())
+    .then(data => {
+      const lastItem = data[data.length - 1]; // Access the last item in the data array
+      props.tasaPlazoHandler(lastItem.v / 100); // Process the last item as needed
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+
+  // Fallback por si falla la API de BCRA.
+  useEffect(() => {
+    props.tasaPlazoHandler(0.9299);
+  },[])
+
+  const plazoFijoHandler = (e) => {
+    props.tasaPlazoHandler(e.target.value / 100)
+  }
+
+  return (
+    <div className={styles.resultado}>
+      <div className={styles.numeros}>
+        <div className={styles["tasa"]}>
+          <p>Tasa de plazo fijo</p>
+          <input type="number" value={props.plazoFijo} onChange={plazoFijoHandler} />
+        </div>
+        <div className={styles["calculo"]}>
+          <p>Valor final:</p>
+          <p className={styles["valor-final"]}>$ {props.valorFinal}</p>
+          <p className={styles["tasa-interes"]}>{props.interes}</p>
+        </div>
+      </div>
+      <div className={styles.texto}></div>
+      {props.resultado}
+    </div>
+  );
+};
+
+export default Result;
