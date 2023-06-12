1571171e741d9cd899e9cab3907f3f999ecf16ea
soype <phenestroza@gmail.com>
2023-06-12 01:00:44 -0300

    Add decoration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Form from "./components/calculator/Form";
 import Stonks from "./assets/Stonks";
 import Tarjeta from "./assets/Tarjeta";
+import Dot from "./assets/Dot";
+import Bar from "./assets/Bar";
 import About from "./components/about/About";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Form></Form>
       <Stonks></Stonks>
       <Tarjeta></Tarjeta>
+      <Dot></Dot>
+      <Bar></Bar>
       <About></About>
     </div>
   );
---
+++ src/assets/Bar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import bar from './bar.png';
+
+const Bar = () => {
+  return (
+    <div className="bar">
+       <img src={bar} alt="" />
+    </div>
+  )
+}
+
+export default Bar
