7b5465dd5ee06599f156890d20196f25ddcf8ba5
Pedro Henestroza <phenestroza@gmail.com>
2025-08-02 10:51:08 -0300

    update tasa

--- src/components/calculator/Result.js
+++ src/components/calculator/Result.js
@@ -7,7 +7,7 @@ import styles from "./Result.module.css";
 const Result = (props) => {
   // Fallback por si falla la API de BCRA.
   useEffect(() => {
-    props.tasaPlazoHandler(0.44);
+    props.tasaPlazoHandler(0.2751);
   }, []);
 
   const plazoFijoHandler = (e) => {
